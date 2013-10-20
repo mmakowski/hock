6cdf7b855ef919a025ddf832d0178f7d8482683c
Maciek Makowski <maciek.makowski@gmail.com>
2013-10-20 13:08:52 +0100

    moved tests to QuickCheck

--- Hock.hs
+++ Hock.hs
@@ -30,13 +30,16 @@ subtree (A _) (A _)   = error $ "unable to take a non-1 subtree of an atom"
 subtree c     _       = error $ "invoked subtree with non-atomic first argument: " ++ (show c)
 
 nock :: Noun -> Noun
-nock (C a (C (C b c) d))       = C (nock (C a (C b c))) (nock (C a d))
-nock (C a (C (A 0)   b))       = subtree b a
-nock (C _ (C (A 1)   b))       = b
-nock (C a (C (A 2)   (C b c))) = nock $ C (nock (C a b)) (nock (C a c))
-nock (C a (C (A 3)   b))       = isCell $ nock (C a b)
-nock (C a (C (A 4)   b))       = inc $ nock (C a b)
-nock (C a (C (A 5)   b))       = isEq $ nock (C a b)
+nock (C a (C (C b c)  d))       = C (nock (C a (C b c))) (nock (C a d))
+nock (C a (C (A  0)   b))       = subtree b a
+nock (C _ (C (A  1)   b))       = b
+nock (C a (C (A  2)   (C b c))) = nock $ C (nock (C a b)) (nock (C a c))
+nock (C a (C (A  3)   b))       = isCell $ nock (C a b)
+nock (C a (C (A  4)   b))       = inc $ nock (C a b)
+nock (C a (C (A  5)   b))       = isEq $ nock (C a b)
+nock (C a (C (A  6)   (C b (C c d)))) = 
+  nock (C a (C (A 2) (C (C yes no) (C (A 2) (C (C no (C c d)) (C (C no yes) (C (A 2) (C (C (A 1) (C (A 2) (A 3))) (C (C no yes) (C (A 4) (C (A 4) b)))))))))))
+nock (C a (C (A  7)   (C b c))) = nock (C a (C (A 2) (C b (C (A 1) c))))
 nock (A n)                     = error $ "*" ++ (show n)
 
 
---
+++ Test.hs
@@ -0,0 +1,39 @@
+module Main where
+import Control.Applicative
+import Test.Framework (Test, defaultMain, testGroup)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck
+import Hock
+
+main :: IO () 
+main = defaultMain tests
+
+instance Arbitrary Noun where
+  arbitrary = oneof [ A <$> arbitrary
+                    , C <$> arbitrary <*> arbitrary
+                    ]
+
+tests = [ testGroup "Nock rewrite rules" [ wholeTreeSelection 
+                                         , leftSubtreeSelection 
+                                         , rightSubtreeSelection
+                                         , deepSubtreeSelection
+                                         , constOp
+                                         ]
+        ]
+wholeTreeSelection = testProperty "whole tree selection" $ \ tree -> 
+  nock (C tree (C (A 0) (A 1))) == tree
+
+leftSubtreeSelection = testProperty "left subtree selection" $ \(left, right) ->
+  let tree = C left right
+  in nock (C tree (C (A 0) (A 2))) == left
+
+rightSubtreeSelection = testProperty "right subtree selection" $ \(left, right) ->
+  let tree = C left right
+  in nock (C tree (C (A 0) (A 3))) == right
+
+deepSubtreeSelection = testProperty "right subtree selection" $ \deep ->
+  let tree = C (C (A 4) deep) (A 3)
+  in nock (C tree (C (A 0) (A 5))) == deep
+
+constOp = testProperty "const operator" $ \(ignored, selected) ->
+  nock (C ignored (C (A 1) selected)) == selected
